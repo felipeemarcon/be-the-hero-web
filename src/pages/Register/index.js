3419394c68f2ac5fa4a539d48586ae8724fcc186
Felipe Marcon <felipeemarcon@gmail.com>
2020-03-27 23:09:43 -0300

    feat: push user to root url after ong register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
 // Services
@@ -18,6 +18,8 @@ export default function Register() {
   const [city, setCity] = useState("");
   const [uf, setUf] = useState("");
 
+  const history = useHistory();
+
   async function handleRegister(e) {
     e.preventDefault();
 
@@ -32,6 +34,8 @@ export default function Register() {
     try {
       const response = await api.post("/ongs", data);
       alert(`Seu ID de acesso: ${response.data.id}`);
+
+      history.push("/");
     } catch (error) {
       alert("Erro no cadastro. Tente novamente");
     }
