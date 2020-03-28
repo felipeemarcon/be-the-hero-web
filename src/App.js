7b802ba3de612daa73d8cc7b15d7bcda85a02978
Felipe Marcon <felipeemarcon@gmail.com>
2020-03-27 22:00:10 -0300

    feat: start use routes component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import React from "react";
 import "./global.css";
 
 // Pags
-import Logon from "./pages/Logon";
+import Routes from "./routes";
 
 function App() {
-  return <Logon />;
+  return <Routes />;
 }
 
 export default App;
