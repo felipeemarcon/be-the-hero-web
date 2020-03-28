b392badce855644a0ec6ac13c05f571f9a12301e
Felipe Marcon <felipeemarcon@gmail.com>
2020-03-27 23:14:44 -0300

    feat: make login with 'sessions' api endpoint

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 
 // Styles
@@ -9,16 +9,43 @@ import "./styles.css";
 import logoImg from "../../assets/logo.svg";
 import herosImg from "../../assets/heroes.png";
 
+// Services
+import api from "../../services/api";
+
 export default function Logon(props) {
+  const [id, setId] = useState("");
+
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+
+      history.push("/profile");
+    } catch (err) {
+      alert("Falha no login. Tente novamente.");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
 
-        <form action="">
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input type="text" placeholder="Sua ID" />
+          <input
+            type="text"
+            value={id}
+            onChange={e => setId(e.target.value)}
+            placeholder="Sua ID"
+          />
           <button className="button" type="submit">
             Entrar
           </button>
